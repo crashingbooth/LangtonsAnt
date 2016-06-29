1e896a16bad3e215b71ab52592433f317e8cd522
crashing booth <jholtzkener@hotmail.com>
2016-06-29 16:01:29 +0900

    Initial Commit

---
+++ Hexagonal Lantons AntTests/Hexagonal_Lantons_AntTests.m
@@ -0,0 +1,39 @@
+//
+//  Hexagonal_Lantons_AntTests.m
+//  Hexagonal Lantons AntTests
+//
+//  Created by Jeff Holtzkener on 6/29/16.
+//  Copyright © 2016 Jeff Holtzkener. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hexagonal_Lantons_AntTests : XCTestCase
+
+@end
+
+@implementation Hexagonal_Lantons_AntTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
